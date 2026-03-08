7fdf9f78b4a48ba565c2f68bb15b10f6c8dcae39
vishwajeetclasses <yt.vishalshirsat@gmail.com>
2026-03-08 18:42:47 +0530

    app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.set_page_config(page_title="Vishwajeet Classes", layout="centered")
 # २. गुगल शीट कनेक्शन
 def load_data():
     # तुमची शीटची लिंक येथे पेस्ट करा
-    sheet_url = "तुमची_पूर्ण_शीट_लिंक_येथे_टाका"
+    sheet_url = "https://docs.google.com/spreadsheets/d/1IMw_nRER8fz-yUtgwKyt12Xmw4ZgwoSh4a19seAFzqc/edit?usp=sharingा"
     
     if "edit" in sheet_url:
         csv_url = sheet_url.split('/edit')[0] + '/export?format=csv'
@@ -74,3 +74,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
